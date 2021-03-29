9c2f8c4df64f42816285584a6c0b93bad4ed4aa4
GucciHsuan <80258372+GucciHsuan@users.noreply.github.com>
2021-03-29 20:18:50 +0800

    Update tracert.py

--- source/functions/tracert.py
+++ source/functions/tracert.py
@@ -2,8 +2,9 @@ import os
 import platform
 
 class traceip():
-    def tracert(target):
+    def tracert(trace): 
+        trace = input('請輸入目標ip或網域 : ')
         if platform.system() == 'Windows':
-            return os.system('tracert '+target)
+            return os.system('tracert '+trace)
         elif platform.system() == 'Linux':
-            return os.system('traceroute '+target)
+            return os.system('traceroute '+trace)
