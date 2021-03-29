a4bb54072964cf1b51de43b55b08a023c0209876
benson871229 <71371929+benson871229@users.noreply.github.com>
2021-03-29 20:59:57 +0800

    nmap_update

--- source/functions/nmap.py
+++ source/functions/nmap.py
@@ -1,15 +1,12 @@
 import os
 import wget
 import subprocess
-#file=wget.download('https://nmap.org/dist/nmap-7.91-setup.exe')
-
-IP=input("輸入ip:")
-nmap= "nmap " + IP  
-#os.system( nmap ) 
-
-subprocess = subprocess.Popen(nmap, shell=True, stdout=subprocess.PIPE)
-subprocess_return = subprocess.stdout.read()
-
-with open("result.txt", "wb") as file:
-    file.write(subprocess_return)
+def nmap():
+    
+    IP=input("輸入ip:")
+    nmap= "nmap " + IP  
+    subprocess = subprocess.Popen(nmap, shell=True, stdout=subprocess.PIPE)
+    subprocess_return = subprocess.stdout.read()
+    with open("result.txt", "wb") as file:
+        file.write(subprocess_return)
 
