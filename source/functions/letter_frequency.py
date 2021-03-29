a4f3d3bbf74535648ffca37f93274eae99ac3f9b
benson871229 <71371929+benson871229@users.noreply.github.com>
2021-03-29 19:46:22 +0800

    Update letter_frequency.py

--- source/functions/letter_frequency.py
+++ source/functions/letter_frequency.py
@@ -5,7 +5,7 @@ def frequency():
     
     str1=input("輸入字串")
     res = ''.join(sorted(str1))  
-    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     chars_in_string = Counter(res)
     res = {}
     for letter in letters:
@@ -13,7 +13,7 @@ def frequency():
             res[letter] = chars_in_string[letter]
         else: 
             res[letter] = 0 
-    print(res)
+    #print(res)
     plt.bar(range(len(res)), list(res.values()), align='center')
     plt.xticks(range(len(res)), list(res.keys()))
 
