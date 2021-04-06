d4094b691af08f5a415c72510ecfd7a776f13013
Carlton Chen <carlton0521@gmail.com>
2021-04-06 21:06:31 +0800

    修改benson871229完成

--- source/functions/nmap.py
+++ source/functions/nmap.py
@@ -1,12 +1,20 @@
+from FuncBase import FuncBase
 import os
 import wget
 import subprocess
-def nmap():
-    
-    IP=input("輸入ip:")
-    nmap= "nmap " + IP  
-    subprocess = subprocess.Popen(nmap, shell=True, stdout=subprocess.PIPE)
-    subprocess_return = subprocess.stdout.read()
-    with open("result.txt", "wb") as file:
-        file.write(subprocess_return)
 
+
+def nmap():
+
+    @property
+    def Description(self):
+        return('Nmap')
+
+    def Run(self):
+        IP = input("輸入ip:")
+        nmap = "nmap " + IP
+        subprocess = subprocess.Popen(nmap, shell=True, stdout=subprocess.PIPE)
+        subprocess_return = subprocess.stdout.read()
+        with open("result.txt", "wb") as file:
+            file.write(subprocess_return)
+        return
